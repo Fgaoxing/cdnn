67e623731bd4d07e3dd160f192492b8c66a34cb3
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-08-02 14:39:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -318,7 +318,7 @@ http.createServer(function (req, res) {
                 //获取npm的数据
                 getNpmData(package,file).then(function (data) {
                     res.writeHead(200, {
-                        'Content-Type': 'text/html;charset=utf-8'
+                        'Content-Type': 'text/plain;charset=utf-8'
                     });
                     res.write(data);
                     res.end();
