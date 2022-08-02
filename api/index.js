8a56ac25141355970270ed70f24cc7c2f236e2f2
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-08-02 14:34:12 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -190,7 +190,7 @@ function getNpmData(packager, file) {
 //路由
 http.createServer(function (req, res) {
     console.log(req.url)
-    req.url=url.parse(req.url).pathname
+    //req.url=url.parse(req.url).pathname
     //路由的路径是/gh/:owner/:repo@:branch/:path
     if (req.url.indexOf('/gh/') === 0) {
         var url = req.url.split('/gh/')[1];
